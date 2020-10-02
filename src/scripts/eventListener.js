ea9b5583be142218981ee06884c6fd5acba227ea
Peter Tran <tran.peter08@gmail.com>
2020-10-02 01:31:34 -0700

    wrote js to verify syntax in vs

--- src/scripts/eventListener.js
+++ src/scripts/eventListener.js
@@ -1,3 +1,30 @@
+function code() {
+  window.addEventListener('message', function ({data}) {
+    // alert(JSON.stringify(data))
+
+    switch (data) {
+      case 'ADD_BID':
+        addBid();
+      default:
+        break;
+    }
+  });
+
+  function addBid() {
+    if (!State.data) {
+      alert('Please select an assignment');
+      return;
+    }
+
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({
+        type: 'ADD_BID',
+        payload: {bidInfo: State.data},
+      })
+    );
+  }
+}
+
 export default `
   window.addEventListener('message', function({data}) {
     // alert(JSON.stringify(data))
