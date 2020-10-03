13b46a9594a8e5b5f99ac3f99b8b4490bf1c9414
Peter Tran <tran.peter08@gmail.com>
2020-10-03 01:49:58 -0700

    created BidList component

---
+++ src/components/BidList.js
@@ -0,0 +1,29 @@
+import React, {useContext} from 'react';
+import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import {MainCtx} from '../screens/Webview';
+import Bid from './Bid';
+
+export default function BidList(props) {
+  const {menuTitle, collection} = useContext(MainCtx);
+  const bids = collection[menuTitle];
+
+  return (
+    <>
+      {bids && bids.length ? (
+        bids.map((bidData) => {
+          console.log({bidData});
+
+          return <Bid key={bidData.bidId} {...bidData} />;
+        })
+      ) : (
+        <Text style={styles.noBids}>- No bids added -</Text>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  noBids: {
+    textAlign: 'center',
+  },
+});
