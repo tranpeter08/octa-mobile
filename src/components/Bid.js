d7c195ccfe215abc8f125dab31617711cd7edfa2
Peter Tran <tran.peter08@gmail.com>
2020-10-03 01:49:41 -0700

    added bid details and remove button

--- src/components/Bid.js
+++ src/components/Bid.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import SimpleButton from './SimpleButton';
+import {MainCtx} from '../screens/Webview';
+import Storage from '../services/Storage';
+import asyncHandlerJSON from '../utils/asyncHandler';
 
-export default function Bid(props) {
+export default function Bid({bidId, daysOff, totalSplit, totalWork}) {
+  const {getAllBids, menuTitle, employeeId} = useContext(MainCtx);
+  function handlePress() {
+    // remove from database and update list
+    Storage.removeBid(employeeId, menuTitle, bidId);
+  }
+
   return (
     <>
-      <Text>bid id</Text>
+      <Text>ID: {bidId}</Text>
+      <Text>Total Work: {totalWork}</Text>
+      <Text>Total Sokit: {totalSplit}</Text>
+      <Text>Days Off: {daysOff.join(', ')}</Text>
+      <SimpleButton title="Remove" onPress={handlePress} />
     </>
   );
 }
