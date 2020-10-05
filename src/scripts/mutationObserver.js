47f31c0d66ad77aa359224b62203409a4acda5d4
Peter Tran <tran.peter08@gmail.com>
2020-10-04 23:24:01 -0700

    converted Observer object

--- src/scripts/mutationObserver.js
+++ src/scripts/mutationObserver.js
@@ -57,59 +57,61 @@ function startObserver() {
 }
 
 export default `
-function startObserver() {
-  window.ReactNativeWebView.postMessage('start observer');
+const Observer = {
+  start() {
+    window.ReactNativeWebView.postMessage('start observer');
 
-  const targetNode = document.getElementById('ApplicationBody');
+    const targetNode = document.getElementById('ApplicationBody');
 
-  if (!targetNode) return;
-  
-  const config = {attributes: false, childList: true, subtree: true};
+    if (!targetNode) return;
+    
+    const config = {attributes: false, childList: true, subtree: true};
 
-  function callback(mutationsList, obsvr) {
-    window.ReactNativeWebView.postMessage('obsvr callback');
+    function callback(mutationsList, obsvr) {
+      window.ReactNativeWebView.postMessage('obsvr callback');
 
-    for (let mutation of mutationsList) {
-      if (
-        mutation.target.id === 'RosterPositionBid' ||
-        mutation.target.id === 'HolidayDetail' ||
-        mutation.target.id === 'OpenAssignmentBidHome' ||
-        mutation.target.className === 'ListBody SortedList FilteredList' ||
-        mutation.target.className === 'ListBody SortedList' ||
-        mutation.target.className === 'ListBody'
-      ) {
-        window.ReactNativeWebView.postMessage('scrape');
-        Utils.configureApp();
-        break;
-      }
+      for (let mutation of mutationsList) {
+        if (
+          mutation.target.id === 'RosterPositionBid' ||
+          mutation.target.id === 'HolidayDetail' ||
+          mutation.target.id === 'OpenAssignmentBidHome' ||
+          mutation.target.className === 'ListBody SortedList FilteredList' ||
+          mutation.target.className === 'ListBody SortedList' ||
+          mutation.target.className === 'ListBody'
+        ) {
+          window.ReactNativeWebView.postMessage('scrape');
+          Utils.configureApp();
+          return;
+        }
 
-      if (
-        mutation.type !== 'childList' ||
-        mutation.target.id !== 'OpenAssignmentBidHome'
-      ) {
-        break;
-      }
-
-      if (!mutation.addedNodes.length) continue;
-
-      let oaDetail;
-
-      for (let {id} of mutation.addedNodes) {
-        if (id === 'OpenAssignmentDetail') {
-          oaDetail = true;
+        if (
+          mutation.type !== 'childList' ||
+          mutation.target.id !== 'OpenAssignmentBidHome'
+        ) {
           break;
         }
-      }
 
-      if (oaDetail) {
-        Utils.configureApp();
-        break;
+        if (!mutation.addedNodes.length) continue;
+
+        let oaDetail;
+
+        for (let {id} of mutation.addedNodes) {
+          if (id === 'OpenAssignmentDetail') {
+            oaDetail = true;
+            break;
+          }
+        }
+
+        if (oaDetail) {
+          Utils.configureApp();
+          return;
+        }
       }
     }
-  }
 
-  const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+  }
 }
 `;
