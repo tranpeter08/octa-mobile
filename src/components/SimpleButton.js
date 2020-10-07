525141ba81eb2dd097c854bee44a6b1a35a1b1f1
Peter Tran <tran.peter08@gmail.com>
2020-10-06 22:18:25 -0700

    added separate props for text and container style

--- src/components/SimpleButton.js
+++ src/components/SimpleButton.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 
-export default function SimpleButton({onPress, style, title}) {
+export default function SimpleButton({
+  onPress,
+  containerStyle,
+  textStyle,
+  title,
+}) {
   return (
     <>
-      <TouchableOpacity onPress={onPress} style={style}>
-        <Text style={{textAlign: 'center'}}>{title}</Text>
+      <TouchableOpacity
+        onPress={onPress}
+        style={[styles.touchable, containerStyle]}
+      >
+        <Text style={[styles.text, textStyle]}>{title}</Text>
       </TouchableOpacity>
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  touchable: {
+    backgroundColor: '#0159c6',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+});
