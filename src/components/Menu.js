af3eb11708c439b619d69b89dfe08f6702bd3ef8
Peter Tran <tran.peter08@gmail.com>
2020-10-13 16:22:04 -0700

    modularized major menu components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,37 +1,31 @@
-import React, {forwardRef, useContext} from 'react';
-import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import React, {useContext} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import MainCtx from '../context/MainCtx';
+import MenuCtx from '../context/MenuCtx';
 import StorageService from '../services/Storage';
-import SimpleButton from './SimpleButton';
 import BidList from './BidList';
-
-/* 
-bid shape: {
-  [employeeId]: {
-    [menu_title]: [bids],
-    [other_menu_title]: [bids]
-  },
-  [other_employeeId]: {
-    [menu_title]: [bids],
-    [other_menu_title]: [bids]
-  }
-}
-
-*/
+import MenuTopNav from './MenuTopNav';
+import MenuControls from './MenuControls';
 
 export default function Menu(props) {
-  const {menuTitle, collection, webView, toggleMenu, employeeId, getAllBids} = useContext(
-    MainCtx
-  );
+  const {
+    menuTitle, 
+    collection, 
+    webView, 
+    employeeId, 
+    getAllBids,
+  } = useContext(MainCtx);
 
   function addBid() {
     webView.current.postMessage('ADD_BID');
   }
 
   async function clearBids() {
-    if (!collection[menuTitle].length) return;
+    const bids = collection[menuTitle];
+    if (!bids || !bids.length) return;
 
-    const [done, err] = await StorageService.clearBids(employeeId, menuTitle, collection);
+    const [done, err] = await StorageService
+      .clearBids(employeeId, menuTitle, collection);
 
     if (err) {
       return alert(err.message);
@@ -41,38 +35,18 @@ export default function Menu(props) {
   }
 
   return (
-    <View style={styles.menu}>
-      <View style={styles.topContainer}>
-        <SimpleButton
-          title="Close"
-          containerStyle={styles.closeButton}
-          onPress={toggleMenu}
-        />
+    <MenuCtx.Provider value={{addBid, clearBids}}>
+      <View style={styles.menu}>
+        <MenuTopNav />
+        <Text style={styles.menuHeading}>{menuTitle} Favorites</Text>
+        <BidList />
+        <MenuControls />
       </View>
-      <Text style={styles.menuHeading}>{menuTitle} Favorites</Text>
-      <BidList />
-      <View style={styles.menuButtonContainer}>
-        <SimpleButton
-          title="Clear"
-          containerStyle={styles.clearButtonContainer}
-          textStyle={styles.clearButtonTextStyle}
-          onPress={clearBids}
-        />
-        <SimpleButton title="Add" onPress={addBid} />
-      </View>
-    </View>
+    </MenuCtx.Provider>
   );
 }
 
-const blue = '#0159c6';
-
 const styles = StyleSheet.create({
-  closeButton: {
-    marginLeft: 'auto',
-  },
-  topContainer: {
-    display: 'flex',
-  },
   menu: {
     width: 240,
     flex: 1,
@@ -87,21 +61,5 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 20,
   },
-  menuButtonContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    margin: 8,
-  },
-  clearButtonContainer: {
-    backgroundColor: '#fff',
-    borderColor: blue,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    marginLeft: 'auto',
-    marginRight: 8,
-  },
 
-  clearButtonTextStyle: {
-    color: blue,
-  },
 });
