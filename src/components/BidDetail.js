6e6b33c201e4e048093576da5581b77cc3542f93
Peter Tran <tran.peter08@gmail.com>
2020-10-13 16:19:38 -0700

    removed unused imports

--- src/components/BidDetail.js
+++ src/components/BidDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function BidDetail({label, text, style}) {
   return (
